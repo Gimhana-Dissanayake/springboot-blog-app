919b27b5608fa293b05db8e055b233198b574c3f
GimhanaDissanayake <gimhanadev@gmail.com>
2022-04-25 18:57:46 +0530

    30. Create Post REST API - Coding

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -32,4 +32,33 @@ public class Post {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
