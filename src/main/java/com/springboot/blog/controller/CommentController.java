6412ae5c6d8be08756d36f0231fbb228c7a8e59e
GimhanaDissanayake <gimhanadev@gmail.com>
2022-05-10 16:23:35 +0530

    56. delete comment REST API

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -7,9 +7,11 @@ import com.springboot.blog.service.CommentService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,25 @@ public class CommentController {
         CommentDto commentDto = commentService.getCommentById(postId, commentId);
 
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable Long postId, @PathVariable Long commentId,
+            @RequestBody CommentDto commentDto) {
+
+        CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<String> deleteComment(@PathVariable(value = "postId") Long postId,
+            @PathVariable(value = "id") Long commentId) {
+
+        commentService.deleteComment(postId, commentId);
+
+        return new ResponseEntity<>("Comment deleted successfully", HttpStatus.OK);
 
     }
 
